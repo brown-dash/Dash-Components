ec9cd456047397fc86e13f55119a04677f6ec3c5
Geireann Lindfield Roberts <60007097+geireann@users.noreply.github.com>
2022-12-26 12:55:16 +0100

    more updates to component library

--- src/components/ColorPicker/ColorPicker.tsx
+++ src/components/ColorPicker/ColorPicker.tsx
@@ -68,8 +68,8 @@ export const ColorPicker = (props: IColorPickerProps) => {
           <GithubPicker
             color={color}
             colors={["FFFFFF", "#F9F6F2", "#E2E2E2", "#D1D1D1", "#737576", "#4b4a4d", "#222021", 
-            '#EB9694', '#FAD0C3', '#FEF3BD', '#C1E1C5', '#BEDADC', '#C4DEF6', '#BED3F3', '#D4C4FB', 
-            'linear-gradient(#065fff, #85c1f9)', 'linear-gradient(#fc00ff, #00dbde)', 'linear-gradient(#2c3e50, #3498db)', 'linear-gradient(#CCCCB2, #757519)', 'linear-gradient(#ee9ca7, #ffdde1)']}
+            '#EB9694', '#FAD0C3', '#FEF3BD', '#C1E1C5', '#BEDADC', '#C4DEF6', '#BED3F3', '#D4C4FB'
+            ]}
             triangle={'hide'}
             onChange={onChangeComplete}
             onChangeComplete={onChangeComplete}
